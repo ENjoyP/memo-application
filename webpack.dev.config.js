8b49cf20e1f263b30e490994cc66231368314473
EnjoyP <tpahdhkspah1@gmail.com>
2017-06-21 17:13:06 +0900

    server, dev server setting

---
+++ webpack.dev.config.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry : [
+        './src/index.js',
+        'webpack-dev-server/client?http://localhost:4000',
+        'webpack/hot/only-dev-server'
+    ],
+
+    output : {
+        path : '/',
+        filename : 'bundle.js'
+    },
+
+    devServer : {
+        hot : true,
+        filename : 'bundle.js',
+        publicPath : '/',
+        historyApiFallback : true,
+        contentBase : './public',
+        proxy : {
+            '**' : 'http://localhost:3000'
+        },
+        stats : {
+            assets : false,
+            colors : true,
+            version : false,
+            hash : false, 
+            timings : false,
+            chunks : false,
+            chunkModules : false
+        }
+    },
+
+    plugins : [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+
+    module : {
+        loaders : [
+            {
+                test : /\.js$/,
+                loaders : ['react-hot-loader', 'babel-loader?' + JSON.stringify({
+                    cacheDirectory : true,
+                    presets : ['es2015', 'react']
+                })],
+                exclude : /node_modules/
+            }
+        ]
+    }
+};
