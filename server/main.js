d7487ea05e67dc7179fe8f1f3d5f080b6f043e27
EnjoyP <tpahdhkspah1@gmail.com>
2017-06-22 11:02:30 +0900

    회원관련 작성

--- server/main.js
+++ server/main.js
@@ -1,19 +1,49 @@
 // Express Server
 import express from 'express';
 import path from 'path';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import session from 'express-session';
 
 import WebpackDevServer from 'webpack-dev-server';
 import webpack from 'webpack';
 
+import api from './routes';
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connection to mongodb server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/memo');
+
 const app = express();
 const port = 3000;
 const devPort = 4000;
+
+/* handle error */
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+/* use session */
+app.use(session({
+    secret : 'CSH12#$',
+    resave : false,
+    saveUninitialized : true
+}));
+
+app.use(morgan('dev')); // HTTP 요청을 로그하는 미들웨어
+app.use(bodyParser.json()); //요청에서 JSON을 파싱할때 사용되는 미들웨어
 
 app.use('/', express.static(path.join(__dirname, './../public')));
 
 app.get('/hello', (req, res) => {
     return res.send('Hello CodeLab');
 });
+
+app.use('/api', api);
 
 app.listen(port, ()=>{
     console.log('Express is listening on port ', port);
---
+++ server/routes/account.js
@@ -0,0 +1,128 @@
+import express from 'express';
+import Account from '../models/account';
+
+const router = express.Router();
+
+/*
+    ACCOUNT SIGNUP : POST /api/account/signup
+    BODY SAMPLE : { "username" : "test", "password" : "test" }
+    ERROR CODE :
+        1 : BAD USERNAME
+        2 : BAD PASSWORD
+        3 : USERNAME EXISTS
+*/
+router.post('/signup', (req, res) => {
+    let usernameRegex = /^[a-z0-9]+$/;
+    let p_username = req.body.username;
+    let p_password = req.body.password;
+
+    if(!usernameRegex.test(p_username)){
+        return res.status(400).json({
+            error : "BAD USERNAME",
+            code : 1
+        });
+    }
+
+    if(p_password.length < 4 || typeof p_password !== "string"){
+        return res.status(400).json({
+            error : "BAD PASSWORD",
+            code : 2
+        });
+    }
+
+    Account.findOne({username : p_username}, (err, exists) => {
+        if(err) throw err;
+        if(exists){
+            return res.status(409).json({
+                error : "USERNAME EXISTS",
+                code : 3
+            });
+        }
+
+        // CREATE ACCOUNT
+        let account = new Account({
+            username : p_username,
+            password : p_password
+        });
+
+        account.password = account.generateHash(account.password);
+
+        // SAVE IN THE DB
+        account.save( err => {
+            if(err) throw err;
+            return res.json({
+                success : true
+            });
+        });
+    });
+    
+});
+
+/*
+    ACCOUNT SIGNIN : POST /api/account/signin
+    BODY SAMPLE : { "username" : "test", "password" : "test" }
+    ERROR CODES  :
+        1 : LOGIN FAILED
+*/
+
+router.post('/signin', (req, res) => {
+    let p_username = req.body.username;
+    let p_password = req.body.password;
+
+    if(typeof p_password !== "string"){
+        return res.status(401).json({
+            error : "LOGIN FAILED",
+            code : 1
+        });
+    }
+
+    Account.findOne({username : p_username}, (err, account) => {
+        if(err) throw err;
+        if(!account) {
+            return res.status(401).json({
+                error : "LOGIN FAILED",
+                code : 1
+            });
+        }
+
+        if(!account.validateHash(p_password)) {
+            return res.status(401).json({
+                error : "LOGIN FAILED",
+                code : 1
+            });
+        }
+
+        let session = req.session;
+        session.loginInfo = {
+            _id : account._id,
+            username : account.username
+        };
+
+        return res.json({
+            success : true
+        });
+    });
+});
+
+/*
+    GET CURRENT USER INFO GET /api/account/getinfo
+*/
+router.get('/getinfo', (req, res) => {
+    if(typeof req.session.loginInfo === "undefined") {
+        return res.status(401).json({
+            error : 1
+        });
+    }
+
+    res.json({info : req.session.loginInfo});
+});
+
+/*
+    LOGOUT : POST /api/account/logout
+*/
+router.post('/logout', (req, res) => {
+    req.session.destroy(err => { if(err) throw err; });
+    return res.json({ success : true });
+});
+
+export default router;
